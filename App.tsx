7f144c7a5134f924ebcdd529fb8bea4dbdb2d497
parthratra11 <parthratra11@gmail.com>
2025-06-07 12:51:13 +0530

    App Initiated

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import './global.css';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <View>
+        <Text className="font-bold"> Placeholder</Text>
+      </View>
+    </>
+  );
+}
