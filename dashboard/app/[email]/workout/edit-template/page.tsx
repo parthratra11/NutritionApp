90d2d8e737881d2456ccd5139cb2b715d178b3b8
parthratra11 <parthratra11@gmail.com>
2025-07-08 16:48:16 +0530

    Edit workout template page updated

--- dashboard/app/[email]/workout/edit-template/page.tsx
+++ dashboard/app/[email]/workout/edit-template/page.tsx
@@ -406,7 +406,7 @@ export default function EditTemplate() {
                                   e.target.value
                                 )
                               }
-                              className="w-full px-3 py-2 rounded-lg bg-white border-0 text-sm text-center text-[#333333] focus:ring-2 focus:ring-red-500"
+                              className="w-full px-3 py-1 rounded-lg bg-white border-0 text-sm text-center text-[#333333] focus:ring-2 focus:ring-red-500"
                               placeholder="Sets"
                             />
                           </div>
@@ -422,7 +422,7 @@ export default function EditTemplate() {
                                   e.target.value
                                 )
                               }
-                              className="w-full px-3 py-2 rounded-lg bg-white border-0 text-sm text-[#333333] focus:ring-2 focus:ring-red-500"
+                              className="w-full px-3 py-1 rounded-lg bg-white border-0 text-sm text-[#333333] focus:ring-2 focus:ring-red-500"
                               placeholder="Reps"
                             />
                           </div>
@@ -438,7 +438,7 @@ export default function EditTemplate() {
                                   e.target.value
                                 )
                               }
-                              className="w-full px-3 py-2 rounded-lg bg-white border-0 text-sm text-[#333333] focus:ring-2 focus:ring-red-500"
+                              className="w-full px-3 py-1 rounded-lg bg-white border-0 text-sm text-[#333333] focus:ring-2 focus:ring-red-500"
                               placeholder="Demo link"
                             />
                           </div>
