80c50d2273ad5519b0e9041210fb7063fd84b215
parthratra11 <parthratra11@gmail.com>
2025-10-01 02:06:43 +0530

    Progress stall - blue marker adde

--- dashboard/app/[email]/workout/session/page.tsx
+++ dashboard/app/[email]/workout/session/page.tsx
@@ -637,7 +637,7 @@ export default function WorkoutSessionPage() {
       } else if (currentTotalVolume < previousTotalVolume) {
         return "border-4 border-red-500";
       } else {
-        return "border-4 border-red-500"; // No progress = red
+        return "border-4 border-blue-500"; // No progress = red
       }
     }
 
@@ -751,7 +751,7 @@ export default function WorkoutSessionPage() {
             </svg>
           </div>
         );
-      } else {
+      } else if (currentTotalVolume < previousTotalVolume) {
         return (
           <div className="w-8 h-8 bg-red-500 rounded-full flex items-center justify-center absolute -top-4 -right-4 z-[5]">
             <svg
@@ -761,6 +761,18 @@ export default function WorkoutSessionPage() {
             >
               <path d="M7 10l5 5 5-5H7z" />
             </svg>
+          </div>
+        );
+      } else {
+        return (
+          <div className="w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center absolute -top-4 -right-4 z-[5]">
+            {/* <svg
+              className="w-5 h-5 text-white"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M7 10l5 5 5-5H7z" />
+            </svg> */}
           </div>
         );
       }
